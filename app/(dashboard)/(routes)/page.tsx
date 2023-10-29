9ef0e8e2d5db7c8ef042cb03a1aa54c32123c930
CleggOh <andrew.clegg84@gmail.com>
2023-10-29 10:06:52 -0400

    Add authentication and initial route setup

--- app/(dashboard)/(routes)/page.tsx
+++ app/(dashboard)/(routes)/page.tsx
@@ -1,5 +1,7 @@
+import { UserButton } from "@clerk/nextjs";
+
 export default function Home() {
   return (
-    <p>This is a protected page</p>
+    <div><UserButton afterSignOutUrl="/"></UserButton></div>
   )
 }
