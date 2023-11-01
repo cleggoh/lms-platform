79490680cbe6bb490498e21af88f803a5b996280
CleggOh <andrew.clegg84@gmail.com>
2023-10-31 21:03:16 -0400

    Add Chapter Description form component using react-quill

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/page.tsx
@@ -5,6 +5,7 @@ import { ArrowLeft, LayoutDashboard } from "lucide-react";
 import Link from "next/link";
 import { redirect } from "next/navigation";
 import ChapterTitleForm from "./_components/chapter-title-form";
+import ChapterDescriptionForm from "./_components/chapter-description-form";
 
 const ChapterIdPage = async ({
   params,
@@ -63,6 +64,11 @@ const ChapterIdPage = async ({
               <h2 className="text-xl">Customize your chapter</h2>
             </div>
             <ChapterTitleForm
+              chapterId={params.chapterId}
+              courseId={params.courseId}
+              initialData={chapter}
+            />
+            <ChapterDescriptionForm
               chapterId={params.chapterId}
               courseId={params.courseId}
               initialData={chapter}
