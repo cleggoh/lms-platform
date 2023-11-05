a25ab3c348030ee231e1763a27d356947dcb9e03
CleggOh <andrew.clegg84@gmail.com>
2023-11-05 00:48:07 -0400

    Add Stripe integration for course purchase

--- app/(course)/courses/[courseId]/chapters/[chapterId]/_components/course-enroll-button.tsx
+++ app/(course)/courses/[courseId]/chapters/[chapterId]/_components/course-enroll-button.tsx
@@ -2,6 +2,9 @@
 
 import { Button } from "@/components/ui/button";
 import { formatPrice } from "@/lib/format";
+import axios from "axios";
+import { useState } from "react";
+import toast from "react-hot-toast";
 
 interface CourseEnrollButtonProps {
   courseId: string;
@@ -12,8 +15,24 @@ export const CourseEnrollButton = ({
   courseId,
   price,
 }: CourseEnrollButtonProps) => {
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleClick = async () => {
+    try {
+      setIsLoading(true);
+
+      const resp = await axios.post(`/api/courses/${courseId}/checkout`);
+
+      window.location.assign(resp.data.url);
+    } catch (error) {
+      toast.error("Something went wrong");
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
   return (
-    <Button className="w-full md:w-auto" size={"sm"}>
+    <Button className="w-full md:w-auto" size={"sm"} onClick={handleClick}>
       Enroll for {formatPrice(price)}
     </Button>
   );
